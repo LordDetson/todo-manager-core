24cc77aa7b40438031fd3c3fe1fee97ae0b476a2
Dmitry Babanin <dmitry.babanin.jazzteam@gmail.com>
2023-02-04 21:25:03 +0400

    Used Spring Boot 3.0.2

--- model/src/main/java/by/babanin/todo/model/Priority.java
+++ model/src/main/java/by/babanin/todo/model/Priority.java
@@ -3,13 +3,12 @@ package by.babanin.todo.model;
 import java.util.Objects;
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
